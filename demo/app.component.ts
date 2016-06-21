3e6f5197b35bc3ff93b5bedf11e1d4d7829bfeff
JayChase <JonathanChase@outlook.com>
2016-06-21 22:11:32 +0900

    setup lite-server for demo app

---
+++ demo/app.component.ts
@@ -0,0 +1,22 @@
+import {Component, OnInit} from '@angular/core';
+import {HTTP_PROVIDERS} from '@angular/http';
+
+@Component({
+  selector: 'useful-app',
+  templateUrl: 'app/app.component.html',
+  styleUrls: ['app/app.component.css'],
+  providers: [],
+  directives: [ ]
+})
+
+export class AppComponent implements OnInit {
+
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+   
+  }
+}
