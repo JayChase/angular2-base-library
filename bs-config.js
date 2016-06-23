1cbee6e7438125df8db637fe7bd4f42bcacfbe00
JayChase <JonathanChase@outlook.com>
2016-06-23 18:26:26 +0900

    fixed up lite-server

--- bs-config.js
+++ bs-config.js
@@ -1,11 +1,22 @@
-//to add back in logging comment out the middleware override
+//set the start directory and file the middleware override will then ensure that all non file routes get redirected back to the SPA
+var startDir = '/demo';
+var startFile = 'index.html';
+var startPath = startDir + '/' + startFile;
+
 module.exports = {
-    logLevel: "silent",
-    browser: "chrome",
-    startPath: "demo/index.html",
+    logLevel: 'silent',
+    browser: 'chrome',
+    startPath: startPath,
     server: {
         middleware: {
-            0: null
+            0: null, //to add back in logging comment this line out
+            1: function (req, res, next) {            
+                if(req.url.indexOf('.') === -1 && req.url.indexOf(startDir) > -1){
+                    req.url = startPath;
+                }
+                
+                return next();
+            }
         }
     }
 };
